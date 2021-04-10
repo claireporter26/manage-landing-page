8a23996c608e24664dd19b36bf20a621b6c28c37
claireporter26 <58444269+claireporter26@users.noreply.github.com>
2021-04-10 16:04:33 +0100

    added in mobile navigation functionality

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,21 @@
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 let navList = document.querySelector('.main-navigation_ul');
+let navListItem = document.querySelectorAll('.main-navigation_li');
+let introImg = document.querySelector('.intro-illustration');
+let body = document.querySelector('.body');
+const greyedOut = document.querySelector('.greyed-out');
+
+
 
 hamburgerIcon.addEventListener("click", function(){
-    navList.classList.toggle('main-navigation_toggle-active')
+    navList.classList.toggle('main-navigation_ul_toggle-active')
+    introImg.classList.toggle('intro-illustration_toggle-active')
+    greyedOut.classList.toggle('greyed-out_toggle-active')
+    
+    // navListItem.classList.add('main-navigation_li_toggle-active')
+    // for (let i = 0; i < navListItem.length; i++) {
+    //     navListItem[i].classList.toggle('main-navigation_li_toggle-active');
+    // }
 })
 
 
