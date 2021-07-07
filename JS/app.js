8edb1cd54ce60f542c779e50ec6fcaa9b3e504bf
claireporter26 <58444269+claireporter26@users.noreply.github.com>
2021-07-07 12:30:05 +0100

    cleaned up code

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,8 @@
+// jQuery code
 
 $(document).ready(function(){
 
-    
-   
     $(".hamburger-icon").click(function(){
-
         $(".main-navigation_ul").toggleClass("main-navigation_ul_toggle-active");
         $(".intro-illustration").toggleClass("intro-illustration_toggle-active");
         $(".greyed-out").toggleClass("greyed-out_toggle-active");
@@ -14,7 +12,6 @@ $(document).ready(function(){
         } else if ($(".hamburger-icon").attr("src") === "./images/icon-close.svg"){
             $(this).attr("src", "./images/icon-hamburger.svg");
         };
-
     });
 
     $('#email-signup-form').validate({
@@ -23,61 +20,21 @@ $(document).ready(function(){
           required: false,
           email: false
         }
-      }
-      
+      } 
     });
 
-    // $("#myform").validate({
-    //   rules: {
-    //     // simple rule, converted to {required:true}
-    //     name: "required",
-    //     // compound rule
-    //     email: {
-    //       required: true,
-    //       email: true
-    //     }
-    //   }
-    // });
-    
     jQuery.extend(jQuery.validator.messages, {
-      
       email: "Please insert a valid email",
-     
     });
 
 });
 
-
-
-// const swiper = new Swiper('.swiper-container', {
-//     // Optional parameters
-//     // direction: 'vertical',
-//     loop: true,
-  
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-  
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-  
-//     // And if we need scrollbar
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-// });
+//swiperJS code
 
 const swiper = new Swiper('.swiper-container', {
     spaceBetween: 20,
-
-    
     slidesPerView: 'auto',
     allowTouchMove: true,
-    
     centeredSlides: true,
     loop: true,
     autoplay: {
@@ -92,40 +49,9 @@ const swiper = new Swiper('.swiper-container', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  });
+});
 
 
-// const hamburgerIcon = document.querySelector('.hamburger-icon');
-// let navListContainer = document.querySelector('.main-navigation--lrg')
-// let navList = document.querySelector('.main-navigation_ul');
-// let navListItem = document.querySelectorAll('.main-navigation_li');
-// let introImg = document.querySelector('.intro-illustration');
-// let body = document.querySelector('.body');
-// const greyedOut = document.querySelector('.greyed-out');
-// const testimonials = document.querySelectorAll('.testimonial');
-
-
-
-// hamburgerIcon.addEventListener("click", function(){
-//     navList.classList.toggle('main-navigation_ul_toggle-active')
-//     // navListContainer.classList.toggle('main-navigation--lrg_toggle-active')
-//     introImg.classList.toggle('intro-illustration_toggle-active')
-//     greyedOut.classList.toggle('greyed-out_toggle-active')
-    
-//     // navListItem.classList.add('main-navigation_li_toggle-active')
-//     // for (let i = 0; i < navListItem.length; i++) {
-//     //     navListItem[i].classList.toggle('main-navigation_li_toggle-active');
-//     // }
-
-//     // console.log(hamburgerIcon.src)
-
-//     if (hamburgerIcon.src === "http://127.0.0.1:5500/images/icon-hamburger.svg"){
-//         hamburgerIcon.src = "http://127.0.0.1:5500/images/icon-close.svg"
-        
-//     } else if (hamburgerIcon.src === "http://127.0.0.1:5500/images/icon-close.svg"){
-//         hamburgerIcon.src = "http://127.0.0.1:5500/images/icon-hamburger.svg" 
-//     }
-// });
 
 
 
